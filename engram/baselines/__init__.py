b51ebdf596fd66d1b1ab8338b910a957b6a7dbf7
kaelvalen <yintsukuyomi@proton.me>
2026-09-09 15:43:53 +0300

    feat: add CompactConvNet2D to baselines module exports

--- engram/baselines/__init__.py
+++ engram/baselines/__init__.py
@@ -1,5 +1,11 @@
 from .cnn_audio import AudioCNNClassifier
+from .cnn_vision import CompactConvNet2D
 from .resnet1d import ResNet1DClassifier
 from .transformer_baseline import TransformerSequenceClassifier
 
-__all__ = ["AudioCNNClassifier", "ResNet1DClassifier", "TransformerSequenceClassifier"]
+__all__ = [
+    "AudioCNNClassifier",
+    "CompactConvNet2D",
+    "ResNet1DClassifier",
+    "TransformerSequenceClassifier",
+]
